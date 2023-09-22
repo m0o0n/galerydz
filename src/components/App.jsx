60c645d2060414bd32d3869f1ddee88344f76ce0
m0o0n <strikunvlad@gmail.com>
2023-09-22 16:36:56 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import Button from "./Button";
+import ImageGallery from "./ImageGallery";
+import SearchBar from "./Searchbar";
+const BASE_URL = 'https://pixabay.com/api/?key=38661528-d945b4671dc1d38083bff1d33&image_type=photo&orientation=horizontal&per_page=12'
+export class App extends Component {
+  state = {
+    gallery: null,
+    err: '',
+    isLoading: true
+  }
+  async fetchImages(params) {
+    const data = await fetch(BASE_URL)
+    return data.json()
+  }
+  componentDidMount() {
+    this.fetchImages().then(({ hits }) => {
+      setTimeout(()=>{
+        this.setState({ gallery: hits, isLoading: false })
+      }, 1000)
+    }).catch((err) => {
+      this.setState({ err, isLoading: false })
+    })
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <SearchBar />
+        {
+          this.state.isLoading
+            ? <span>Loading</span>
+            : (
+              <>
+                <ImageGallery gallery={this.state.gallery} />
+                <Button isRender={this.state.gallery.length} />
+              </>
+            )
+        }
+
+      </div>
+    );
+  }
+
 };
